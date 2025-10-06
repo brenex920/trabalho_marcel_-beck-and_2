8f69b3503f00feeef6cd9b951511fe3c340fbef4
Breno Machado <machado.breno127@gmail.com>
2025-10-06 00:30:54 -0300

    funçoes comcluidas

--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -22,7 +22,7 @@ const getContadosAllDados = function() {
 
 const message = {status: true, statuscode: 200, development: 'Breno Dias Machado', users : []}
 
-    dados.contatos['whats-users'].forEach(function(item){
+    dados.contatos['whats-users'].forEach((item) => {
         
         let dadosUser = {}
 
@@ -33,14 +33,153 @@ const message = {status: true, statuscode: 200, development: 'Breno Dias Machado
         dadosUser.foto = item['profile-image']
         dadosUser.numero = item.number
         dadosUser.cor_fundo = item.background
+        dadosUser.contatos = item.contacts
         
         message.users.push(dadosUser)
     }) 
     
-    return message
+    return message;
 
 
 }
 
-console.log(getContadosAllDados())
+const getDadosPerfill = function(number){
+
+    const message = {status: true, statuscode: 200, development: 'Breno Dias Machado',}
+
+
+    let numero = number
+
+
+    let profile = dados.contatos['whats-users'].find((number) => {
+
+        return number.number === numero
+
+    })
+        if(profile){
+            let perfill_json = {}
+
+            perfill_json.nickname = profile.nickname
+            perfill_json.foto = profile.foto
+            perfill_json.numero = profile.number
+            perfill_json.cor_fundo = profile.background
+            perfill_json.criado = profile['created-since'].start
+            perfill_json.fim = profile['created-since'].end
+
+            message.perfil = perfill_json
+            return message
+        }else{
+            return MESSAGE_ERROR;
+        }
+
+}
+
+const getDadosContato = function(numero){
+
+    let telefone = numero
+
+     const message = {status: true, statuscode: 200, development: 'Breno Dias Machado'}
+
+  let profile = dados.contatos['whats-users'].find((item)  =>  {
+
+    return item.number === telefone;
+
+  })
+  if(profile){
+    let array = []
+
+    profile.contacts.forEach((item) => {
+        let json2= {}
+
+        json2.nome = item.name
+        json2.numero = item.number
+        json2.descricao = item.description
+
+       array.push(json2);
+    })
+     message.contacts = array;
+      return message;
+  }else{
+    return MESSAGE_ERROR;
+  }
+}
+
+const getContatosInformaçoes = function(numero){
+
+    let message = []
+
+      let profile = dados.contatos['whats-users'].find((item)  =>  {
+
+    return item.number === numero;
+
+  })
+  if(profile){
+
+    dados.contatos['whats-users'].forEach((item) => {
+        item.contacts.forEach((contato) => {
+            message.push({
+                nome: contato.name,
+                number: contato.number,
+                foto: contato.image,
+                patepapo: contato.messages
+            })
+        })
+    })
+    return message
+  }
+}
+
+    
+
+const getMensagens = function(numero) {
+
+    let persona = numero
+
+     const message = {status: true, statuscode: 200, development: 'Breno Dias Machado'}
+
+
+     let men = dados.contatos['whats-users'].find((perfil) => {
+        return perfil.contacts.some(item => item.number === persona)
+     })
+     let array = []
+     if(men){
+        let getNumero = men.contacts.find((item) => {
+            return item.number === persona
+        })
+        json3 = {}
+        json3.nome = getNumero.name
+        json3.message = getNumero.messages
+
+        array.push(json3)
+
+        message.contacts = json3
+        return message
+     }
+}
+
+const getFiltroMessage = function(palavra){
+
+    let messages = []
+
+    dados.contatos['whats-users'].forEach((item) =>{
+        item.contacts.forEach((contato) => {
+            contato.messages.forEach((message) => {
+                if(message.content.includes(palavra)){
+                    messages.push({
+                        name: contato.name,
+                        number: contato.number,
+                        message: message.content
+                    })
+                }else{
+                    return MESSAGE_ERROR
+                }
+            })
+        })
+    })
+    return messages;
+}
+
+
+
+console.log(getContatosInformaçoes('11987876567'));
 
